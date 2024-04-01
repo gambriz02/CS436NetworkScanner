fcc53f588b28aa6c216dbf4329015e89b14cd10b
AirBreather02 <gabriel.ambriz.02@gmail.com>
2024-03-31 22:39:52 -0700

    performs scan of network and displays raw ip and mac data

--- netscan.py
+++ netscan.py
@@ -1,1 +1,17 @@
+from scan import *
+
 #Implementation of main! call scan functions and GUI
+def main():
+    
+    router_ip = sr1(IP(dst="www.google.com", ttl=0)/ICMP()/"XXXXXXXXXXX", verbose=False).src
+
+    host_ip = router_ip+"/24"
+
+    print("Your router ip is: ", router_ip)
+
+    result = scan(host_ip)
+
+    display(result)
+
+if __name__ == "__main__":
+    main()
--- scan.py
+++ scan.py
@@ -1,3 +1,23 @@
-#function for discovering devices on network and sending packets
+from scapy.all import ARP, Ether, srp, sr1, IP, ICMP
 
-#funciton for parsing data from requests
+def scan(ip):
+    print("performing scan")
+
+    arp = ARP(pdst=ip)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether/arp
+
+    result = srp(packet, timeout=3, verbose=False)[0]
+
+    clients = []
+
+    for elem in result:
+        client_dict = {"ip": elem[1].psrc, "mac": elem[1].hwsrc}
+        clients.append(client_dict)
+    
+    return clients
+
+def display(result):
+    for row in result:
+        print(row)
+
